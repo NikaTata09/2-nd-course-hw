75757379819e1df9406425019973155bdc898407
NikaTata09 <Olyniatar@mail.ru>
2023-04-28 15:07:59 +0200

    Выполнила Задание 7

---
+++ script.js
@@ -0,0 +1,32 @@
+//Задание 1
+let a = 10;
+alert (a);
+a = 20;
+alert (a);
+// Задание 2
+let year = 2007;
+alert (`Год выпуска айфона - ${year}`);
+//Задание 3
+let name = ('Brandan');
+alert (`Создатель джава скрипт - ${name}`);
+//Задание 4
+let x = 10;
+let y = 2;
+alert (`сумма чисел - ${x+y}\n вычитание - ${x-y}\n умножение - ${x*y}\n деление - ${x/y}\n`)
+// Задание 5
+let result = 2**5;
+alert (result);
+// Задание 6
+let f = 9;
+let b = 2;
+alert (f/b);
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /=3;
+num ++;
+num--;
+alert (num);
+// 
