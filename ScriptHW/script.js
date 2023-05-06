20961573153354631b7fe65799cfd3e9c716a476
NikaTata09 <Olyniatar@mail.ru>
2023-05-06 18:09:14 +0200

    Изменила конструкцию на else if

--- ScriptHW/script.js
+++ ScriptHW/script.js
@@ -124,22 +124,22 @@ if (Number.isNaN(+number)) {
 let clientOS=Number (prompt ('Введите 0 если у вас IOS или 1 если у вас Android'));
 if(clientOS===0){
     console.log('Установите версию приложения для iOS по ссылке');
-} else{
+} else {
     console.log('Неверное значение переменной clientOS');
 }
 
 //Задание 9
 let clientDeviceYear=Number(prompt('Введите год выпуска телефона?'));
-if(clientOS===0){
+ if(clientOS===0){
     if(clientDeviceYear>=2015){
         console.log('Установите версию приложения для iOS по ссылке');
     } else{
         console.log('Установите облегченную версию приложения для iOS по ссылке');
     }
-} else if(clientOS===1) {
+} else if(clientOS===1){
     if (clientDeviceYear>=2015){
     console.log('Установите версию приложения для Android по ссылке');
-} else{
+} else if (clientDeviceYear<=2015){
     console.log('Установите облегченную версию приложения для Android по ссылке'); 
 } else{
     console.log('Неверное значение переменной clientOS');
